f7cd14cc6e534c569790cfee4434599b3f676184
Pattrick Hueper <phueper@hueper.net>
2016-11-22 17:13:01 +0100

    eslint --fix config files

--- karma.conf.js
+++ karma.conf.js
@@ -1,63 +1,62 @@
-module.exports = function(config) {
-    
-    var saucelabsLaunchers = {
+module.exports = function (config) {
+    const saucelabsLaunchers = {
         sl_ie_9: {
             base: 'SauceLabs',
             browserName: 'internet explorer',
             platform: 'Windows 7',
-            version: '9'
+            version: '9',
         },
         sl_chrome: {
             base: 'SauceLabs',
             browserName: 'chrome',
             platform: 'Windows 10',
-            version: 'latest'
+            version: 'latest',
         },
         sl_firefox: {
             base: 'SauceLabs',
             browserName: 'firefox',
             platform: 'Windows 10',
-            version: 'latest'
+            version: 'latest',
         },
         // doesn't work yet
         sl_safari: {
             base: 'SauceLabs',
             browserName: 'safari',
             platform: 'OS X 10.11',
-            version: 'latest'
+            version: 'latest',
         },
     };
-    
+
     config.set({
         files: [
-            {pattern: 'test/karmaWebpackTestEntry.js'}
+            { pattern: 'test/karmaWebpackTestEntry.js' },
         ],
         frameworks: [
             'mocha',
-            'chai'
+            'chai',
         ],
         preprocessors: {
-            'test/karmaWebpackTestEntry.js': ['webpack']
+            'test/karmaWebpackTestEntry.js': ['webpack'],
         },
         webpack: require('./webpack.config.js'),
         webpackMiddleware: {
-            noInfo: true
+            noInfo: true,
         },
         sauceLabs: {
             testName: 'js-joda karma Tests',
             recordVideo: false,
             recordScreenshots: false,
             connectOptions: {
-                logfile: 'sauce_connect.log'
-            }
+                logfile: 'sauce_connect.log',
+            },
         },
         customLaunchers: saucelabsLaunchers,
         browserDisconnectTimeout: 10000, // default 2000
         // browserDisconnectTolerance: 1, // default 0
-        browserNoActivityTimeout: 4 * 60 * 1000, //default 10000
-        captureTimeout: 4 * 60 * 1000, //default 60000
+        browserNoActivityTimeout: 4 * 60 * 1000, // default 10000
+        captureTimeout: 4 * 60 * 1000, // default 60000
         reporters: ['progress'],
         browsers: ['Chrome', 'Firefox', 'PhantomJS'],
-        plugins: ['karma-*']
+        plugins: ['karma-*'],
     });
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,52 @@
-var fs = require('fs');
-var path = require('path');
-var webpack = require('webpack');
+let fs = require('fs');
+let path = require('path');
+let webpack = require('webpack');
 
-var minify = JSON.parse(process.env.DIST_MIN || '0');
-var sourceMaps = !minify;
+let minify = JSON.parse(process.env.DIST_MIN || '0');
+let sourceMaps = !minify;
 
-function createBanner(){
-    var packageJson = require('./package.json');
-    var version = '//! @version ' + packageJson.name + ' - ' + packageJson.version + '\n';
-    var preamble = fs.readFileSync('./src/license-preamble.js', 'utf8');
+function createBanner() {
+    let packageJson = require('./package.json');
+    let version = `//! @version ${  packageJson.name  } - ${  packageJson.version  }\n`;
+    let preamble = fs.readFileSync('./src/license-preamble.js', 'utf8');
     return version + preamble;
 }
 
-var banner = createBanner();
+let banner = createBanner();
 
 module.exports = {
     context: __dirname,
     entry: './src/js-joda.js',
     devtool: sourceMaps ? 'hidden-source-map' : '',
     output: {
-        path: __dirname  + '/dist',
+        path: `${__dirname   }/dist`,
         filename: minify ? 'js-joda.min.js' : 'js-joda.js',
         libraryTarget: minify ? 'var' : 'umd',
-        library: 'JSJoda'
+        library: 'JSJoda',
     },
     module: {
         loaders: [{
             loader: 'babel-loader',
             include: [
                 path.resolve(__dirname, 'src'),
-                path.resolve(__dirname, 'test')
+                path.resolve(__dirname, 'test'),
             ],
-            test: /.js$/
-        }]
+            test: /.js$/,
+        }],
     },
     plugins: minify ? [
         new webpack.optimize.UglifyJsPlugin({
             comments: false,
             compress: {
-                warnings: false
-            }
+                warnings: false,
+            },
         }),
         new webpack.BannerPlugin(
-            banner, {raw: true}
-        )
+            banner, { raw: true },
+        ),
     ] : [
         new webpack.BannerPlugin(
-            banner, {raw: true}
-        )
-    ]
+            banner, { raw: true },
+        ),
+    ],
 };
